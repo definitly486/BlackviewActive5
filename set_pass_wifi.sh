47ee94a789a13a64cca1bf11e3d5540a414f8cc0
Your Name <you@example.com>
2026-09-22 04:27:10 +0300

    изменено:      list_for_remove 	изменено:      set_pass_wifi.sh

--- set_pass_wifi.sh
+++ set_pass_wifi.sh
@@ -1,2 +1,3 @@
 adb shell cmd wifi connect-network "HUAWEI-B315-AFCA" wpa2 "HR63B1DMTJ4"
-adb shell settings put global low_power 1   
+adb shell settings put global low_power 1
+adb shell pm disable-user --user 0 com.google.android.gms
